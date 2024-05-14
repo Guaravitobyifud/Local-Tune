97ce9fe208a2ffa39d3d2ea3504abf982a9e3b98
Guaravitobyifud <135897855+Guaravitobyifud@users.noreply.github.com>
2024-05-14 17:13:32 -0300

    14/05/2024(2)

--- src/controllers/ControllerPesquisa.js
+++ src/controllers/ControllerPesquisa.js
@@ -1,32 +1,63 @@
 const { connSequelize, BD } = require('../../config/coneccao')
-const {Model, Op, Sequelize} = require('sequelize')
-const {tb_contato} = require ('../models/modeloContato')
-const {tb_endereco} = require ('../models/modeloEndereco')
-const {tb_tipoMusical} = require ('../models/modeloTipoMusical')
-const {tb_tipoUsuario} = require ('../models/modeloTipoUsuario')
-const {tb_usuario} = require ('../models/modeloUsuario')
+const { Model, Op, Sequelize } = require('sequelize')
+const { tb_contato } = require('../models/modeloContato')
+const { tb_endereco } = require('../models/modeloEndereco')
+const { tb_tipoMusical } = require('../models/modeloTipoMusical')
+const { tb_tipoUsuario } = require('../models/modeloTipoUsuario')
+const { tb_usuario } = require('../models/modeloUsuario')
+const { tb_usuTipoMus } = require('../models/modeloUsuTipoMus')
 
 
 exports.Pesquisa = async (req, res) => {
     const { busca } = req.body;
     try {
-        const resultBuscaUsu2 = await tb_usuario.findAll({
+        const resultBuscaUsu = await tb_usuTipoMus.findAll({
             attributes: [
-                [Sequelize.literal('tb_usuario.nm_usuario'), "Usuario"],
-                [Sequelize.literal('tb_tipoMusical.ds_descricaoTpMusical'), 'Tipo_Musical'],
-                [Sequelize.literal('tb_contato.nr_celular'), "Numero_de_celular"],
-                [Sequelize.literal('tb_endereco.nm_endereco'), "Endereco"]
+                [Sequelize.literal('nm_usuario'), "Usuario"],
+                [Sequelize.literal('nr_celular'), "Numero_de_celular"],
+                [Sequelize.literal('nm_endereco'), "Endereco"]
             ],
             include: [
                 {
-                    model: tb_contato,
-                    required: false,
-                    attributes: []
+                    model: tb_usuario,
+                    required: true,
+                    attributes: [],
+                    include: [
+                        {
+                            model: tb_contato,
+                            required: true,
+                            attributes: []
+                        },
+                        {
+                            model: tb_endereco,
+                            required: true,
+                            attributes: []
+                        },
+                    ],
+                    where: {
+                        nm_usuario: {
+                            [Op.like]: `${busca}%`
+                        },
+                    },
                 },
+            ],
+            raw: true
+        });
+
+        const resultBuscaUsu2 = await tb_usuTipoMus.findAll({
+            attributes: [
+                [Sequelize.literal('ds_descricaoTpMusical'), 'Tipo_Musical'],
+            ],
+            include: [
                 {
-                    model: tb_endereco,
-                    required: false,
-                    attributes: []
+                    model: tb_usuario,
+                    required: true,
+                    attributes: [],
+                    where: {
+                        nm_usuario: {
+                            [Op.like]: `${busca}%`
+                        },
+                    },
                 },
                 {
                     model: tb_tipoMusical,
@@ -34,14 +65,10 @@ exports.Pesquisa = async (req, res) => {
                     attributes: []
                 },
             ],
-            where: {
-                nm_usuario: {
-                    [Op.like]: `${busca}%`
-                },
-            },
             raw: true
-        });
-        res.render('Search', { message: resultBuscaUsu2 });
+        })
+
+        res.render('Search', { message: resultBuscaUsu, message2: resultBuscaUsu2});
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
@@ -49,5 +76,5 @@ exports.Pesquisa = async (req, res) => {
 
     }
 
-    
+
 };
